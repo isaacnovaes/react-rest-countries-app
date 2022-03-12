eeb32b1b1b6ce79738b6df41af3561df1c703e39
Isaac Novaes <isaac.novaes@hotmail.com>
2022-03-12 22:38:43 +0100

    Add initial fetch error handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, useCallback } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import styles from "./App.module.scss";
 import appContext from "./context/app-context.js";
 import Header from "./components/Header/Header";
@@ -9,28 +9,30 @@ import { formatCountries } from "./helpers/helperFunctions.js";
 import NoDataFound from "./components/NoDataFound/NoDataFound";
 
 function App() {
+	const [fetchError, setFetchError] = useState(false);
+
 	const context = useContext(appContext);
 
 	const [showLoading, setShowLoading] = useState(true);
 
 	const { updateCountries } = useContext(appContext);
 
-	const fetchCountries = useCallback(async () => {
+	const fetchCountries = async () => {
 		const request = await fetch("https://restcountries.com/v3.1/all");
+
+		if (!request.ok) {
+			setFetchError(true);
+			return;
+		}
 		const data = await request.json();
 		const allCountries = data.map(formatCountries);
 		updateCountries(allCountries);
 		setInterval(() => setShowLoading(false), 2000);
-	}, [updateCountries]);
+	};
 
 	useEffect(() => {
 		fetchCountries();
-	}, [fetchCountries]);
-
-	// the useEffect hook has the fetchCountries dependency
-	// however, fetchCountries is recreated in every render cycle
-	// to avoid this performance issue, fetchCountries should be wrapped in the useCallback hook
-	// this way, the fetchCountries is not recreated in every render cycle
+	}, []);
 
 	const appClasses = `${styles.App} ${
 		context.theme === "dark" ? styles.AppDark : ""
@@ -39,17 +41,21 @@ function App() {
 	return (
 		<div className={appClasses}>
 			<Header />
-			<main>
-				<Routes>
-					<Route path="/" element={<Navigate replace to="/countries" />} />
-					<Route
-						path="/countries"
-						element={<Countries isLoading={showLoading} />}
-					/>
-					<Route path="/countries/:country" element={<Country />} />
-					<Route path="/*" element={<NoDataFound type="countries" />} />
-				</Routes>
-			</main>
+			{fetchError && <NoDataFound type="fetch" />}
+
+			{!fetchError && (
+				<main>
+					<Routes>
+						<Route path="/" element={<Navigate replace to="/countries" />} />
+						<Route
+							path="/countries"
+							element={<Countries isLoading={showLoading} />}
+						/>
+						<Route path="/countries/:country" element={<Country />} />
+						<Route path="/*" element={<NoDataFound type="countries" />} />
+					</Routes>
+				</main>
+			)}
 		</div>
 	);
 }
