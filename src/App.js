42d925be24ccf572138192ae08cd47ae67b4d4a9
Isaac Novaes <isaac.novaes@hotmail.com>
2022-03-12 21:24:20 +0100

    Improve render performance

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useCallback } from "react";
 import styles from "./App.module.scss";
 import appContext from "./context/app-context.js";
 import Header from "./components/Header/Header";
@@ -6,22 +6,31 @@ import Countries from "./pages/Countries/Countries.js";
 import Country from "./pages/Country/Country.js";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { formatCountries } from "./helpers/helperFunctions.js";
+import NoDataFound from "./components/NoDataFound/NoDataFound";
 
 function App() {
 	const context = useContext(appContext);
 
+	const [showLoading, setShowLoading] = useState(true);
+
 	const { updateCountries } = useContext(appContext);
 
-	const fetchCountries = async () => {
+	const fetchCountries = useCallback(async () => {
 		const request = await fetch("https://restcountries.com/v3.1/all");
 		const data = await request.json();
 		const allCountries = data.map(formatCountries);
 		updateCountries(allCountries);
-	};
+		setInterval(() => setShowLoading(false), 2000);
+	}, [updateCountries]);
 
 	useEffect(() => {
 		fetchCountries();
-	}, []);
+	}, [fetchCountries]);
+
+	// the useEffect hook has the fetchCountries dependency
+	// however, fetchCountries is recreated in every render cycle
+	// to avoid this performance issue, fetchCountries should be wrapped in the useCallback hook
+	// this way, the fetchCountries is not recreated in every render cycle
 
 	const appClasses = `${styles.App} ${
 		context.theme === "dark" ? styles.AppDark : ""
@@ -33,8 +42,12 @@ function App() {
 			<main>
 				<Routes>
 					<Route path="/" element={<Navigate replace to="/countries" />} />
-					<Route path="/countries" element={<Countries />} />
+					<Route
+						path="/countries"
+						element={<Countries isLoading={showLoading} />}
+					/>
 					<Route path="/countries/:country" element={<Country />} />
+					<Route path="/*" element={<NoDataFound type="countries" />} />
 				</Routes>
 			</main>
 		</div>
